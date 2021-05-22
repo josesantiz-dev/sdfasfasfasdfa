7f57827f28e090678aa566fa392699759f11d00e
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2021-05-22 13:30:04 -0500

    primer mensaje commit

---
+++ app/src/main/java/com/codersof/xcvdfgdfgdf/MainActivity.java
@@ -0,0 +1,19 @@
+package com.codersof.xcvdfgdfgdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        jbsjas
+                njajjusaf
+                nadjfnjas
+                        nasusida
+
+    }
+}
