c30e933a1dd194b398074849bfbf51f1a13dfef1
José Sántiz <36577849+JOSEPH1990a@users.noreply.github.com>
2022-04-22 19:41:06 -0500

    Add files via upload

---
+++ Conexion.php
@@ -0,0 +1,33 @@
+<?php
+class Conexion{
+	//private $conect;
+	public function __construct(){
+        /*foreach (conexiones as $key => $conexion) {
+            try{
+                $this->conect[$key] = new PDO("mysql:host=".$conexion['DB_HOST'].";dbname=".$conexion['DB_NAME'].";charset=".$conexion['DB_CHARSET'],$conexion['DB_USER'],$conexion['DB_PASSWORD']);
+                $this->conect[$key]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch (PDOException $e){
+                $this->conect[$key] = 'Error de conexión';
+                echo "ERROR: " . $e->getMessage();
+  
+            }
+        }*/
+	}
+
+	/*public function conect($bd){
+		return $this->conect[$bd];
+	}*/
+    public function conect($bd){
+        try{
+            $c = new PDO("mysql:host=".conexiones[$bd]['DB_HOST'].";dbname=".conexiones[$bd]['DB_NAME'].";charset=".conexiones[$bd]['DB_CHARSET'],conexiones[$bd]['DB_USER'],conexiones[$bd]['DB_PASSWORD']);
+            $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $c;
+        }catch(PDOException $e){
+            $msg = 'Error de conexión';
+            return $msg;
+            echo "ERROR: " . $e->getMessage();
+        }
+    }
+}
+
+?>
---
+++ Mysql.php
@@ -0,0 +1,81 @@
+<?php
+
+	class Mysql extends Conexion
+	{
+
+        //public $conexion = [];
+		private $strquery;
+		private $arrValues;
+	    function __construct()
+		{
+            /*foreach (conexiones as $key => $conexion) {
+                $this->conexion[$key] = new Conexion();
+                $this->conexion[$key] = $this->conexion[$key]->conect($key);
+            }
+            var_dump($this->conexion);*/
+		}
+        public function conexion($conn){
+            $conne = new Conexion();
+            $conne = $conne->conect($conn);
+            return $conne;
+        }
+
+		//Insertar un registro
+		public function insert(string $query,string $bd, array $arrValues)
+		{
+			$this->strquery = $query;
+			$this->arrValues = $arrValues;
+			$insert = $this->conexion($bd)->prepare($this->strquery);
+			$resInsert = $insert->execute($this->arrValues);
+			if ($resInsert)
+			{
+				$lastInsert = $this->conexion($bd)->lastInsertId();
+			}else{
+				$lastInsert = 0;
+			}
+			return $lastInsert;
+		}
+
+		//Buscar un registro
+		public function select(string $query, string $bd)
+		{
+			$this->strquery = $query;
+			$result = $this->conexion($bd)->prepare($this->strquery);
+			$result->execute();
+			$data = $result->fetch(PDO::FETCH_ASSOC);
+			return $data;
+		}
+
+		//Devuelve todos los registros
+		public function select_all(string $query, string $bd)
+		{
+			//$bd = "conexion".$base_datos;
+			$this->strquery = $query;
+			$result = $this->conexion($bd)->prepare($this->strquery);
+			$result->execute();
+			$data = $result->fetchall(PDO::FETCH_ASSOC);
+			return $data;
+		}
+
+		//Actualizar registros
+		public function update(string $query, string $bd, array $arrValues)
+		{
+			$this->strquery = $query;
+			$this->arrValues = $arrValues;
+			$update = $this->conexion($bd)->prepare($this->strquery);
+			$resExecute = $update->execute($this->arrValues);
+			return $resExecute;
+		}
+
+		//Eliminar un registro
+		public function delete(string $query,string $bd)
+		{
+			$this->strquery = $query;
+			$result = $this->conexion($bd)->prepare($this->strquery);
+			$del = $result->execute();
+			return $del;
+		}
+        
+	}
+
+?>
